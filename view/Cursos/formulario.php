946c8e6a9eff2264b0b83ad0f5424288bd01a557
Daniela Poletto <danielapoletto_bep@hotmail.com>
2022-05-15 15:59:19 -0300

    Entendendo Modelo View Controller | Understanding Model View Controller

---
+++ view/Cursos/formulario.php
@@ -0,0 +1,11 @@
+
+<?php include __DIR__.'/../inicio-html.php'; ?>        
+    <form>
+        <div class="form-group">
+            <label for="descricao">Descrição</label>
+            <input type="text" id="descricao" name="descricao" class="form-control">
+        </div>
+    </form>
+    <button class="btn btn-primary">Salvar</button>
+<?php include __DIR__.'/../fim-html.php'; ?>
+        
