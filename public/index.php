5f2bda5c8a031c511d17ddc64254068db0b9ceb1
Daniela Poletto <danielapoletto_bep@hotmail.com>
2022-05-15 15:07:00 -0300

    Uma entrada para requisições | single entry for requests

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+switch ($_SERVER['PATH_INFO']) {
+    case '/listar-cursos':
+        require 'listar-cursos.php';
+        break;
+    
+    case '/novo-curso':
+        require 'formulario-novo-curso.php';
+        break;
+    
+    default:
+        echo "Erro 404";
+}
+?>
--- public/listar-cursos.php
+++ public/listar-cursos.php
@@ -17,6 +17,11 @@ $cursos = $repositorioDeCursos->findAll();
     <div class="jumbotron">
         <h1>Listar cursos</h1>
     </div>
+
+    <a href="/novo-curso" class="btn btn-primary mb-2">
+        Novo Curso
+    </a>
+    
     <ul class="list-group">
         <?php foreach ($cursos as $curso): ?>
             <li class="list-group-item">
