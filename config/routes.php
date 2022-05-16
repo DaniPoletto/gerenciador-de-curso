81d35ae37584747c1d50c42d4e1d9e30cef25257
Daniela Poletto <danielapoletto_bep@hotmail.com>
2022-05-16 20:43:11 -0300

    update

--- config/routes.php
+++ config/routes.php
@@ -4,10 +4,8 @@ use Alura\Cursos\Controller\ListarCursos;
 use Alura\Cursos\Controller\Persistencia;
 use Alura\Cursos\Controller\FormularioInsercao;
 
-$rotas = [
+return [
     '/listar-cursos' => ListarCursos::class,
     '/novo-curso' => FormularioInsercao::class,
     '/salvar-curso' => Persistencia::class
 ];
-
-return $rotas;
