f205b5e4aeead931a8fd90037dd763d82b391208
Daniela Poletto <danielapoletto_bep@hotmail.com>
2022-05-17 20:53:28 -0300

    excluir curso | delete course

--- config/routes.php
+++ config/routes.php
@@ -1,11 +1,14 @@
 <?php
 
-use Alura\Cursos\Controller\ListarCursos;
-use Alura\Cursos\Controller\Persistencia;
-use Alura\Cursos\Controller\FormularioInsercao;
+use Alura\Cursos\Controller\{
+    Exclusao,
+    ListarCursos,
+    Persistencia,
+    FormularioInsercao};
 
 return [
     '/listar-cursos' => ListarCursos::class,
     '/novo-curso' => FormularioInsercao::class,
-    '/salvar-curso' => Persistencia::class
+    '/salvar-curso' => Persistencia::class,
+    '/excluir-curso' => Exclusao::class
 ];
--- view/Cursos/listar-cursos.php
+++ view/Cursos/listar-cursos.php
@@ -5,8 +5,9 @@
     
     <ul class="list-group">
         <?php foreach ($cursos as $curso): ?>
-            <li class="list-group-item">
+            <li class="list-group-item d-flex justify-content-between">
                 <?= $curso->getDescricao(); ?>
+                <a href="/excluir-curso?id=<?=$curso->getId();?>" class="btn btn-danger btn-sm">Excluir</a>
             </li>
         <?php endforeach; ?>
     </ul>
